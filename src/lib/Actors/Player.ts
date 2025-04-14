0bcf919e4690dc90e8a1d293cbeeb4c721ade698
Bhvya123 <bhvyakothari13@gmail.com>
2025-04-14 22:26:40 +0530

    reverted player changes

--- src/lib/Actors/Player.ts
+++ src/lib/Actors/Player.ts
@@ -2,9 +2,6 @@ import { Scene } from "phaser";
 import { SceneObject } from "../SceneObject";
 import { Transform } from "../Behaviours/Transform";
 import { SpriteRenderer } from "../Behaviours/SpriteRenderer";
-import PlayerControls from "../../controls/PlayerControls";
-import { KeyboardControls } from "../../controls/KeyboardControls";
-import { IControls } from "../../controls/IControls";
 
 export class Player extends SceneObject {
     public speed: number = 5;
@@ -15,13 +12,12 @@ export class Player extends SceneObject {
 
     private currentSpeed: Phaser.Math.Vector2;
 
-    // private readonly leftKey: Phaser.Input.Keyboard.Key | undefined;
-    // private readonly rightKey: Phaser.Input.Keyboard.Key | undefined;
-    // private readonly downKey: Phaser.Input.Keyboard.Key | undefined;
-    // private readonly upKey: Phaser.Input.Keyboard.Key | undefined;
+    private readonly leftKey: Phaser.Input.Keyboard.Key | undefined;
+    private readonly rightKey: Phaser.Input.Keyboard.Key | undefined;
+    private readonly downKey: Phaser.Input.Keyboard.Key | undefined;
+    private readonly upKey: Phaser.Input.Keyboard.Key | undefined;
 
     private currentAnim: string = "idle";
-    private controls: PlayerControls;
     
     constructor(scene: Scene) {
         super(scene);
@@ -33,12 +29,11 @@ export class Player extends SceneObject {
 
         this.sprite = new SpriteRenderer(this, "gold-knight");
         this.addBehaviour(this.sprite);
-        
-        this.controls = new PlayerControls(new KeyboardControls(scene));
-        // this.leftKey = scene.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        // this.rightKey = scene.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-        // this.downKey = scene.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
-        // this.upKey = scene.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+
+        this.leftKey = scene.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        this.rightKey = scene.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        this.downKey = scene.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+        this.upKey = scene.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
     }
 
     public onStart(): void {
@@ -49,54 +44,27 @@ export class Player extends SceneObject {
 
     public onTick(): void {
 
-        const direction = this.controls.getDirection();
-        if (direction.length() === 0){
-            this.currentSpeed.x = this.currentSpeed.y = 0;
-            this.sprite.anims?.pause();
-        } else{
-            if(direction.x < 0) {
-                this.currentAnim = 'left';
-                this.currentSpeed.x = -1;
-            }
-            if(direction.x > 0) {
-                this.currentAnim = 'right';
-                this.currentSpeed.x = 1;
-            }
-            if(direction.y < 0) {
-                this.currentAnim = 'up';
-                this.currentSpeed.y = -1;
-            }
-            if(direction.y > 0) {
-                this.currentAnim = 'down';
-                this.currentSpeed.y = 1;
-            }
-            this.sprite.anims?.play(this.currentAnim, true);
-        }
+        this.currentSpeed.x = this.currentSpeed.y = 0;
 
-        // this.currentSpeed.x = this.currentSpeed.y = 0;
+        if(this.leftKey?.isDown) this.currentSpeed.x -= 1;
+        if(this.rightKey?.isDown) this.currentSpeed.x += 1;
+        if(this.downKey?.isDown) this.currentSpeed.y += 1;
+        if(this.upKey?.isDown) this.currentSpeed.y -= 1;
 
-        // if(this.leftKey?.isDown) this.currentSpeed.x -= 1;
-        // if(this.rightKey?.isDown) this.currentSpeed.x += 1;
-        // if(this.downKey?.isDown) this.currentSpeed.y += 1;
-        // if(this.upKey?.isDown) this.currentSpeed.y -= 1;
+        this.currentSpeed.normalize();
 
-        // this.currentSpeed.normalize();
+        if(this.currentSpeed.length() == 0) this.sprite.anims?.pause();
 
-        // if(this.currentSpeed.length() == 0) this.sprite.anims?.pause();
+        if(this.currentSpeed.x < 0) this.currentAnim = 'left';
+        if(this.currentSpeed.x > 0) this.currentAnim = 'right';
+        if(this.currentSpeed.y < 0) this.currentAnim = 'up';
+        if(this.currentSpeed.y > 0) this.currentAnim = 'down';
 
-        // if(this.currentSpeed.x < 0) this.currentAnim = 'left';
-        // if(this.currentSpeed.x > 0) this.currentAnim = 'right';
-        // if(this.currentSpeed.y < 0) this.currentAnim = 'up';
-        // if(this.currentSpeed.y > 0) this.currentAnim = 'down';
-
-        // this.sprite.anims?.play(this.currentAnim, true);
+        this.sprite.anims?.play(this.currentAnim, true);
 
         this.transform.position.x += this.currentSpeed.x * this.speed;
         this.transform.position.y += this.currentSpeed.y * this.speed;
 
         super.onTick();
-    }
-    public setControlStrategy(strategy: IControls) {
-        this.controls.setStrategy(strategy);
     }
 }
